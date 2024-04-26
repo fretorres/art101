f353a97800b4ca2e67e744b80072f41f9b458398
fretorres <frederica.torresf@gmail.com>
2024-04-25 18:11:46 -0700

    lab 5
    iufhawif

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,13 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - 
+//rock paper scissors
+//An implementation of the game
+// Author: Freddie Torres
+// Date: April 23, 2024
 
 // Constants
-
+Rock
+Paper 
+Scissors
 // Functions
+//Print the characters of rock paper and scissors
+//Print a score table, with a section for each the player and the computer
+//Identify variables 
+  //Rock
+  // Paper
+  //Scissors.
+//Allow Rock to beat Scissors, Paper to beat Rock, and Scissors to beat paper.
+//Allow players to select Rock, Paper or Scissors.
+//Confirm collection
+//Allow computer to randomly select Rock, Paper or scissors with a 33% probability for each
+//Reveal players selection and computer selection after a 3 second countdown
+//Award Points
+  //If players selection beats computers selection, increase players point tally +1
+  //If computers selection beats players selection, increase computers point tally +1
+  //If computers selection is the same as players selection, do not increase either point tally
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+function myFunction(param1, param2) {              
   // some code here
   // return results;
 }
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5, Data Types and Variables
+// Author: Freddie Torrea
+// Date: April 24, 2024
 
 // Constants
 
+
+  
+//Define variables
+make = "Volkswagen";
+model= "Golf";
+color = "grey";
+year = 2005;
+
+//Calculate
+age = 2024 - year
+
 // Functions
+
+//output
+  document.writeln("Make:" + make + "<br>");
+  document.writeln("Model:" + model + "<br>");
+  document.writeln("Color:" + color + "<br>");
+  document.writeln("Year:" + year + "<br>");
+  document.writeln("Age:" + age + "<br>");
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
